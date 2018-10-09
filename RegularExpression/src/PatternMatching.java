d3ac13c534502ff560d784e03b73ca3b386e9d58
trinadhreddy63 <trinadhreddy63@gmail.com>
2018-10-09 14:40:15 +0530

    regular expression1

--- RegularExpression/src/PatternMatching.java
+++ RegularExpression/src/PatternMatching.java
@@ -1,6 +1,6 @@
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
+//hello
 public class PatternMatching {
     public static void main(String[] args)
     {
--- RegularExpression/src/StringTokenizer1.java
+++ RegularExpression/src/StringTokenizer1.java
@@ -2,7 +2,7 @@ import java.util.StringTokenizer;
 public class StringTokenizer1 {
     public static void main(String[] args)
     {
-        StringTokenizer s=new StringTokenizer("Durga Software Solutions");
+        StringTokenizer s=new StringTokenizer("27-04-2017","-");
         while(s.hasMoreTokens())
         {
             System.out.println(s.nextToken());
