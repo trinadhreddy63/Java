d4baf0d78ebd695f7157b0bbd55f08335c7efeee
trinadhreddy63 <trinadhreddy63@gmail.com>
2018-10-10 15:40:16 +0530

    recursion function factorial

---
+++ RegularExpression/src/recursionFunctionFactorial.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+    val number = 5
+    val result: Long
+    result = factorial(number)
+    println("Factorial of $number = $result")
+}
+
+fun factorial(n: Int): Long {
+    return if(n == 1){
+        n.toLong()
+    }
+    else{
+        n*factorial(n-1)
+    }
+}
