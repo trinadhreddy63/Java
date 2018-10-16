11af923e830da74a50fe16037cd00319f609359c
trinadhreddy63 <trinadhreddy63@gmail.com>
2018-10-16 13:21:55 +0530

    collection functions

--- RegularExpression/src/Replace.kt
+++ RegularExpression/src/Replace.kt
@@ -1,9 +1,24 @@
 import java.*
+import java.time.*
 import java.util.*
 
     fun main(args: Array<String>) {
         val list = Arrays.asList(*"one Two three Four five six one three Four".split(" ".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray())
         println("List :$list")
-        Collections.replaceAll(list, "one", "hundread")
+        Collections.replaceAll(list, "one", "hundred")
         println("replaceAll: $list")
+        val currentZone = ZoneId.systemDefault()
+        println(currentZone)
+        val localDateTime = LocalDateTime.of(2016, 12, 18, 12, 34)
+        println(localDateTime)
+        val hereDateTime = ZonedDateTime.of(localDateTime, currentZone)
+        println(hereDateTime)
+        val offset = hereDateTime.getOffset()
+        println(offset)
+        val allZoneIds = ZoneId.getAvailableZoneIds()
+        var count=1
+        for(i in allZoneIds) {
+            println("$count  $i")
+            count++
+        }
     }
